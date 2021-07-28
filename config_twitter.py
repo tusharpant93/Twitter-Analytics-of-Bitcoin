e56d5c32c80af2c145d9c36994027d4a70a31969
Tushar Pant <72465037+tusharpant93@users.noreply.github.com>
2021-07-28 16:52:34 -0400

    Add files via upload

---
+++ config_twitter.py
@@ -0,0 +1,10 @@
+## Twitter API credentials.
+
+# Consumer keys (API key and secret)
+consumer_key = 'cl8xqISjRrUurXxgqhkuYEz2u'
+consumer_secret = 'vCW0BqCPrQ5LQc6rAxxVbkDqIFL33tRtfzpIYc12uhV7idhN9M'
+
+# Application access token key and access token secret
+access_token = '1414953849639612420-8IAO6k9yDjJLZ2hPUvaGq5kojzmO4r'
+access_token_secret = 'he31BxzYJlHgYZYVx70sWOibnynLJopERopOjY4SdSbeX'
+
